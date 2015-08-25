a86e5220dea3bef8ad8188231716424f2e2d7140
Shota Hirao <shota.hirao@gmail.com>
2015-08-25 14:19:10 +0900

    Add comment

--- demoapp/AppDelegate.m
+++ demoapp/AppDelegate.m
@@ -22,6 +22,7 @@
     
     // インストール後の初回起動時にCircuitサーバーと通信し、インストール前の各種情報を取得するために必要な処理
     // ※以下の処理は初回起動時のみ動きます。NSUserDefaultへ初回起動なのかどうかの状態を保存します。アプリを削除すると初回判定の情報も消えます。
+    // ※FingerPringting方式
     [CircuitDirect setAppId:@"da87802909047102" callback:^(NSError *error) {
         if ( error ) {
             NSLog(@"error: %@", error);
